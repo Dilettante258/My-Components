7ca976af732a6d7aada0d6d51c72fc90c433df2c
Dilettante258 <2226519330@qq.com>
2024-04-16 13:33:15 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import dayjs from "dayjs";
+import Calendar from "./Calendar";
+
+function App() {
+  return (
+    <div className="App">
+      <Calendar
+        value={dayjs("2023-11-08")}
+        onChange={(date) => {
+          alert(date.format("YYYY-MM-DD"));
+        }}
+      ></Calendar>
+    </div>
+  );
+}
+
+export default App;
